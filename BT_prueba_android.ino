df4995a21fa966e0eb44e566c37d22a06d881ca3
ETR <49214324+etorresr@users.noreply.github.com>
2022-05-16 19:11:35 -0500

    Update BT_prueba_android.ino

--- BT_prueba_android.ino
+++ BT_prueba_android.ino
@@ -2,30 +2,25 @@
 BluetoothSerial SerialBT;
 bool men = false;
 String message1 = "";
-String mess = "e";
+String mess = "combinacion recibida";
 
 
 void setup() {
-  // put your setup code here, to run once:
 Serial.begin(115200);
-SerialBT.begin("ESP32pumitas"); //Bluetooth device name
+SerialBT.begin("ESP32pumitas"); 
   Serial.println("The device started, now you can pair it with bluetooth!");
   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 if (SerialBT.available()) {
-    
     char incomingChar1 = SerialBT.read();
-    
     if (incomingChar1!= '\n'){
       message1 += String(incomingChar1);
-      //Serial.println(message1);
     }
     else{
-      men=true;
-      
+      men=true; 
     }
   }
   if (men){
